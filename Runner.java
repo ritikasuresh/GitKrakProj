9d5b43f140503cb75cc558b9eb144cb1848c8591
Ritika Suresh <1219ritika@gmail.com>
2019-06-07 10:19:38 -0400

    Combined all work into one single file

--- Runner.java
+++ Runner.java
@@ -1,7 +1,14 @@
 public class Runner{
 	public Runner(){
 
+		float a = (float)(Math.random()*10)+0;
+		float b = (float)(Math.random()*10)+0;
+		DecimalFormat df = new DecimalFormat("0.000");
+		PrintQuotientInterface quot = (float x, float y) -> System.out.printf("The quotient of %s and %s is %s.%n", df.format(a), df.format(b), df.format(a/b));
+		quot.printQuot(a, b);
 
+		Perfect_Square sqRoot = (double n) -> Math.sqrt(n)%1 == 0;
+		System.out.println(sqRoot.detectSquare(25));
 
 	}
 
